aa59dd5365b04365e875051a2dd51d9bad8b5c5a
Zygy93 <amazinzygy@gmail.com>
2018-03-05 15:44:54 -0600

    close

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,48 @@
 var app = angular.module( 'myApp', [ 'ngRoute' ] );
+console.log( 'in the MyApp' );
+const ChatController = myApp.controller( 'ChatController', ['$http', function($http){
+    self.messages = MessageService.messages;
+    self.submitButton = MessageService.submitButton;
+    self.getAllMessages = MessageService.getAllMessages;
+    
+    let self = this;
 
-console.log( 'NG' );
+    self.messages = {newMessage:{}, allMessages:[]};
+
+    self.submitButton = function(newMessage){
+        console.log('in submitButton function');
+        $http({
+            method: 'POST',
+            url: '/message',
+            data: {
+                name: newMessage.name,
+                message: newMessage.message
+            }
+          }).then(function(response) {
+            self.messages.newMessage = {};
+            console.log('Sent new message');
+            self.getAllMessages();
+          })
+          .catch(function(error){
+            console.log(error);
+          })
+    }
+
+    self.getAllMessages = function(){
+        console.log('In getAllMessages function');
+        $http({
+            method: 'GET',
+            url: '/message'
+        }).then(function(response){
+            console.log('Got all messages:', response.data);
+            self.messages.allMessages = response.data; 
+            console.log('allMessages array:', self.messages.allMessages);
+        }).catch(function(error){
+            console.error('Error getting messages', error)
+        })
+    }
+
+    self.getAllMessages();
+
+  }]);
+  
--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
+app.use(express.static('server/public/'));
+
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // needed for angular requests
 
-// middleware
-app.use( express.static( 'server/public' ) );
-app.use( bodyParser.json() );
+// Mongoose setup ------------------------------------
+const mongoose = require('mongoose');
 
-let port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log( 'server running on:', port );
+const databaseUrl = 'mongodb://localhost:27017/ccfive_james';
+
+mongoose.connection.on('connected', function() {
+  console.log('mongoose connected to: ', databaseUrl);
+})
+
+mongoose.connection.on('error', function(error) {
+  console.log('mongoose connection error: ', error);
+})
+
+mongoose.connect(databaseUrl);
+// End Mongoose Setup -------------------------------
+
+//Router
+const messageRouter = require('./routes/messageRouter');
+app.use('/ccfive_james', messageRouter);
+
+
+//Start up the server
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
 });
